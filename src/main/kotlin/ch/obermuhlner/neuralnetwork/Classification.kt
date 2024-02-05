b60c764ece2ef8c7fb18253c086165550747b7e5
Eric Obermühlner <eobermuhlner@gmail.com>
2024-02-05 08:45:56 +0100

    add utility methods for classification

---
+++ src/main/kotlin/ch/obermuhlner/neuralnetwork/Classification.kt
@@ -0,0 +1,21 @@
+package ch.obermuhlner.neuralnetwork
+
+object Classification {
+
+    fun toOutputMatrix(label: Int, labelCount: Int): Matrix {
+        return Matrix(labelCount, 1, {index -> if (index == label) 1.0 else 0.0 })
+    }
+
+    fun toOutputLabel(m: Matrix): Int {
+        var result = 0
+        var resultValue = 0.0
+        for (index in 0 until m.size) {
+            val value = m[index]
+            if (value > resultValue) {
+                result = index
+                resultValue = value
+            }
+        }
+        return result
+    }
+}
